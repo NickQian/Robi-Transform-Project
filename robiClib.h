5c386bf2a547b40cdaff3525f376b83e25fcce25
NickQian <btltzster@gmail.com>
2016-01-28 11:38:53 +0800

    Create robiClib.h

---
+++ robiClib.h
@@ -0,0 +1,9 @@
+#include "Python.h"
+
+#ifndef __CPU_RPI2B__    //__CPU_Exynos4412__
+ #define __CPU_RPI2B__   //__BCM2836__   
+#endif
+
+//#define __DEBUG__        //
+#define __PWM_SERIALISE_MODE__
+
